66fd03137083ffb1f17f07c54a5fe69c40ccf1a0
Justice-love <eddyxu1213@126.com>
2017-05-02 20:29:27 +0800

    增加映射类+修改xml

---
+++ plugin/src/main/java/org/eddy/xml/data/KeyColumn.java
@@ -0,0 +1,18 @@
+package org.eddy.xml.data;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created by eddy on 2017/5/2.
+ */
+@Getter
+@Setter
+@ToString
+public class KeyColumn {
+
+    private String column;
+
+    private String javaType;
+}
