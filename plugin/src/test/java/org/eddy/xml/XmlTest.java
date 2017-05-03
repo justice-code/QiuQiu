1b241fd3c6bc1f3a5978beac91180ee27f302b98
Justice-love <eddyxu1213@126.com>
2017-05-03 16:55:53 +0800

    xml解析类测试

---
+++ plugin/src/test/java/org/eddy/xml/XmlTest.java
@@ -0,0 +1,19 @@
+package org.eddy.xml;
+
+import org.eddy.xml.context.XmlDataContext;
+import org.eddy.xml.data.DataNode;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by eddy on 2017/5/3.
+ */
+public class XmlTest {
+
+    @Test
+    public void test() {
+        List<DataNode> dataNodes = XmlDataContext.getContext().getNodes();
+    }
+
+}
