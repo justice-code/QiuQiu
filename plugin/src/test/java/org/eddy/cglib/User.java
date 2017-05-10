805fb059e11ab78fe113f8c0c35242d240f3a1f1
Justice-love <eddyxu1213@126.com>
2017-05-10 09:00:31 +0800

    增加测试内容

--- plugin/src/test/java/org/eddy/cglib/User.java
+++ plugin/src/test/java/org/eddy/cglib/User.java
@@ -3,6 +3,10 @@ package org.eddy.cglib;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by eddy on 2017/5/9.
  */
@@ -18,5 +22,16 @@ public class User {
     public int setAndGetAge(int age) {
         this.age = age;
         return age;
+    }
+
+    private void test() {
+
+    }
+
+    public static void main(String[] args) {
+        Method[] methods =  User.class.getMethods();
+        Arrays.stream(methods).forEach(m -> {
+            System.out.println(m.getName());
+        });
     }
 }
