710035aae500b99421555faf4da73dc1ea48b6af
Justice-love <eddyxu1213@126.com>
2017-06-06 17:41:43 +0800

    增加切面

---
+++ plugin/src/main/java/org/eddy/aop/MapperAspect.java
@@ -0,0 +1,24 @@
+package org.eddy.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by eddy on 2017/6/6.
+ */
+@Aspect
+@Component
+public class MapperAspect {
+
+    @Pointcut("execution(* org.eddy.dao.mapper..*(..))")
+    public void mapperCheck() {}
+
+    @Around("mapperCheck()")
+    public Object check(ProceedingJoinPoint point) throws Throwable {
+        System.out.println("into");
+        return point.proceed();
+    }
+}
