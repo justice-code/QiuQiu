c1772fea5435b3af70b7e3d739609c71409e2ee1
Justice-love <eddyxu1213@126.com>
2017-06-11 22:39:56 +0800

    增加单元测试

--- plugin/src/test/java/org/eddy/ognl/OgnlTest.java
+++ plugin/src/test/java/org/eddy/ognl/OgnlTest.java
@@ -6,7 +6,9 @@ import org.eddy.xml.context.XmlDataContext;
 import org.eddy.xml.data.RuleNode;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by eddy on 2017/6/9.
@@ -17,6 +19,15 @@ public class OgnlTest {
     public void test() throws OgnlException {
         List<RuleNode> dataNodes = XmlDataContext.getContext().getNodes();
         Object value = Ognl.getValue("[0].keyColumn.table", dataNodes);
+        System.out.println(value);
+    }
+
+    @Test
+    public void test2() throws OgnlException {
+        List<RuleNode> dataNodes = XmlDataContext.getContext().getNodes();
+        Map<String, List<RuleNode>> map = new HashMap<>();
+        map.put("key", dataNodes);
+        Object value = Ognl.getValue("key[1].keyColumn.table", map);
         System.out.println(value);
     }
 }
