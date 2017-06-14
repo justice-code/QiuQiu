27a3550a6e3ccc87eebda376e878d09224d8016a
Justice-love <eddyxu1213@126.com>
2017-06-14 09:36:04 +0800

    重建引用

--- apply/src/main/java/org/eddy/aop/MineMapperAspect.java
+++ apply/src/main/java/org/eddy/aop/MineMapperAspect.java
@@ -1,7 +1,6 @@
 package org.eddy.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
